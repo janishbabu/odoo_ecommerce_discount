93dfdbe30c78b29ef9a90426fd122252dcfa1706
janishbabuek <janishbabuek@gmail.com>
2023-03-24 16:52:09 +0530

    ecommerce_discount

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "E-commerece Discount",
+    'version': "16.0.1.0.0",
+    'sequence': -13,
+    'author': 'CYBROSYS',
+    'installable': True,
+    'application': True,
+    'depends': ['base','website_sale','sale_management'],
+    'data': ['data/discount.xml'
+             ]
+}
+
